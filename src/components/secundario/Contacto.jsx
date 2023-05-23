8460b25fad1969e795f3a9949c780e4fc323104b
rexdaro <rexdaro@gmail.com>
2023-05-23 13:41:27 -0300

    footer listo

--- src/components/secundario/Contacto.jsx
+++ src/components/secundario/Contacto.jsx
@@ -1,6 +1,16 @@
 import mail from '../../../public/icon/mail_ico.svg'
-import foto_manu from '../../../public/img/foto_manu.jpg'
+import foto_manu from '../../../public/img/foto_manu2.jpg'
+import github from '../../../public/icon/github_cuadrado_blanco.svg'
+import linkedin from '../../../public/icon/linkedinBlanco.svg'
+import whatsapp from '../../../public/icon/whatsapp.svg'
+import gmail from '../../../public/icon/mailCuadradoBlanco_ico.svg'
+import instagram from '../../../public/icon/instagram.svg'
+import facebook from '../../../public/icon/facebook.svg'
+import youtube from '../../../public/icon/youtube.svg'
+import twitch from '../../../public/icon/twitch.svg'
+import cv from '../../../public/icon/cv.svg'
 
+import Icono from './componentes_iconos/Icono'
 
 function Contacto({clase}) {
 
@@ -16,7 +26,7 @@ function Contacto({clase}) {
                     <h5>¡Hablemos!</h5>
 
                     <div className='div_boton'>
-                        <a className='boton_enviar' href="mailto:rexdaro@gmail.com">
+                        <a className='boton_enviar' href="https://mail.google.com/mail/?view=cm&to=rexdaro@gmail.com" target="_blank">
                             <img className='ico_mail' src={mail} alt="mail" />
                             <h2 >
                                 Contactame aquí
@@ -33,6 +43,12 @@ function Contacto({clase}) {
 
 
                 <div className='iconos_footer'>
+                    <a href="https://github.com/rexdaro" target="_blank"><Icono claseImg='img_icono_footer' icono={github} descripcion={''}/></a>
+                    <a href="https://www.linkedin.com/in/manuel-rivas-a56709200/" target="_blank"><Icono claseImg='img_icono_footer' icono={linkedin} descripcion={''}/></a>
+                    <a href="https://wa.me/542617047380" target="_blank"><Icono claseImg='img_icono_footer' icono={whatsapp} descripcion={''}/></a>
+                    <a href="https://mail.google.com/mail/?view=cm&to=rexdaro@gmail.com" target="_blank"><Icono claseImg='img_icono_footer' icono={gmail} descripcion={''}/></a>
+                    <a href="https://www.instagram.com/manuel_rivasb/" target="_blank"><Icono claseImg='img_icono_footer' icono={instagram} descripcion={''}/></a>
+                    <a href="https://drive.google.com/file/d/1yBJctyop7aLnEl0EgNDHkVs5wDyZZzqp/view?usp=sharing" target="_blank"><Icono claseImg='img_icono_footer' icono={cv} descripcion={''}/></a>
 
                 </div>
 
